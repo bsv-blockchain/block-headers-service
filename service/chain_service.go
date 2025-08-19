22a0e024cdf7eccafe5e3a565275c88dce97e4e7
Kuba <127198012+kuba-4chain@users.noreply.github.com>
2025-08-19 17:04:24 +0200

    fix(#339): fix one-block fork issue (#342)

--- service/chain_service.go
+++ service/chain_service.go
@@ -142,10 +142,13 @@ func (cs *chainService) switchChainsStates(h *domains.BlockHeader) error {
 		return ChainUpdateFail.causedBy(&err)
 	}
 
-	err = cs.Headers.UpdateState(headerStaleChain.hashes(), domains.LongestChain)
-	if err != nil {
-		return ChainUpdateFail.causedBy(&err)
+	if headerStaleChain != nil {
+		err = cs.Headers.UpdateState(headerStaleChain.hashes(), domains.LongestChain)
+		if err != nil {
+			return ChainUpdateFail.causedBy(&err)
+		}
 	}
+
 	return nil
 }
 
@@ -205,7 +208,7 @@ type chain []*domains.BlockHeader
 
 func lowestHeightOf(c *chain, oh *domains.BlockHeader) int32 {
 	f := c.first()
-	if f.Height < oh.Height {
+	if f != nil && f.Height < oh.Height {
 		return f.Height
 	}
 	return oh.Height
