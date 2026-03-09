e47442e30c16ced11de9758333d5f6ef40383061
Mr. Z <mrz1836@users.noreply.github.com>
2026-03-09 13:23:25 -0400

    refactor(addrmgr): pre-allocate prefix buffer with capacity
    Use make() with capacity to avoid potential reallocations when appending data to the prefix slice.

--- transports/p2p/addrmgr/addrmanager.go
+++ transports/p2p/addrmgr/addrmanager.go
@@ -504,7 +504,8 @@ func (a *AddrManager) HostToNetAddress(host string, port uint16, services wire.S
 		if err != nil {
 			return nil, err
 		}
-		prefix := []byte{0xfd, 0x87, 0xd8, 0x7e, 0xeb, 0x43}
+		prefix := make([]byte, 6, 6+len(data))
+		copy(prefix, []byte{0xfd, 0x87, 0xd8, 0x7e, 0xeb, 0x43})
 		ip = net.IP(append(prefix, data...))
 	} else if ip = net.ParseIP(host); ip == nil {
 		ips, err := a.lookupFunc(host)
